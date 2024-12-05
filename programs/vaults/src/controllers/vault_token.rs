bbcd3e5f54d3a8b1e3dadc03c2cde56845bc2600
Sounak Pradhan <22390515+sounak98@users.noreply.github.com>
2024-12-05 16:33:34 +0100

    fix: bugs related to deposit and withdraw

--- programs/vaults/src/controllers/vault_token.rs
+++ programs/vaults/src/controllers/vault_token.rs
@@ -4,8 +4,8 @@ use anchor_spl::token::*;
 use crate::state::Vault;
 
 pub fn mint_vault_token<'info>(
-    vault: Account<'info, Vault>,
-    mint: Account<'info, Mint>,
+    vault: Box<Account<'info, Vault>>,
+    mint: Box<Account<'info, Mint>>,
     to: Account<'info, TokenAccount>,
     amount: u64,
     signer_seeds: &[&[&[u8]]],
@@ -24,18 +24,20 @@ pub fn mint_vault_token<'info>(
 }
 
 pub fn burn_vault_token<'info>(
-    vault: Account<'info, Vault>,
-    mint: Account<'info, Mint>,
+    user: Signer<'info>,
+    mint: Box<Account<'info, Mint>>,
     from: Account<'info, TokenAccount>,
     amount: u64,
+    signer_seeds: &[&[&[u8]]],
     token_program: Program<'info, Token>,
 ) -> Result<()> {
     let burn_accounts = Burn {
         mint: mint.to_account_info(),
         from: from.to_account_info(),
-        authority: vault.to_account_info(),
+        authority: user.to_account_info(),
     };
-    let cpi_ctx = CpiContext::new(token_program.to_account_info(), burn_accounts);
+    let cpi_ctx =
+        CpiContext::new_with_signer(token_program.to_account_info(), burn_accounts, signer_seeds);
     burn(cpi_ctx, amount)?;
 
     Ok(())
