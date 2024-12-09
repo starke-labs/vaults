0b814bb2be1cce31b2a5da40a2fd162de2281874
Sounak Pradhan <22390515+sounak98@users.noreply.github.com>
2024-12-09 20:00:00 +0100

    feat: add security txt

--- programs/vaults/src/constants.rs
+++ programs/vaults/src/constants.rs
@@ -1,4 +1,3 @@
 use anchor_lang::prelude::*;
 
-// TODO: What happens when we need to redeploy the program?
 pub static PROGRAM_AUTHORITY: Pubkey = pubkey!("STRK1me6eFLDYGKYqbn2oyHsaxiCHe8GDWQnnSGiScS");
