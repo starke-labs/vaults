76223b19b02776c44378d439aae72135ceb2e977
Sounak Pradhan <22390515+sounak98@users.noreply.github.com>
2024-12-16 11:05:17 +0100

    feat: implement token price retrieval from Pyth feed
    - Added a new module `token_price` to handle fetching token prices from the Pyth price feed.
    - Introduced a constant `PYTH_PRICE_FEED_MAX_AGE_SECONDS` to define the maximum age for price updates.
    - Updated the `test_pyth_price_feed` function to utilize the new `get_token_price_from_pyth_feed` function for improved price retrieval logic.
    - Removed the direct price fetching logic from `test_pyth_price_feed` for better modularity.

--- programs/vaults/src/constants.rs
+++ programs/vaults/src/constants.rs
@@ -1,3 +1,5 @@
 use anchor_lang::prelude::*;
 
 pub static PROGRAM_AUTHORITY: Pubkey = pubkey!("STRK1me6eFLDYGKYqbn2oyHsaxiCHe8GDWQnnSGiScS");
+
+pub static PYTH_PRICE_FEED_MAX_AGE_SECONDS: u64 = 30;
