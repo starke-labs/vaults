bb73cc21f58fcee860ca39cc08144c9246fa67aa
Sounak Pradhan <22390515+sounak98@users.noreply.github.com>
2025-01-20 14:21:00 +0100

    chore: use PRECISION instead of DECIMALS

--- programs/vaults/src/constants.rs
+++ programs/vaults/src/constants.rs
@@ -6,3 +6,5 @@ pub static PROGRAM_AUTHORITY: Pubkey = pubkey!("STRK1me6eFLDYGKYqbn2oyHsaxiCHe8G
 pub static PYTH_PRICE_FEED_MAX_AGE_SECONDS: u64 = 60;
 
 pub static NAV_DECIMALS: u8 = 9;
+
+pub static PRECISION: u64 = 10u64.pow(NAV_DECIMALS as u32);
--- programs/vaults/src/controllers/token_price.rs
+++ programs/vaults/src/controllers/token_price.rs
@@ -1,7 +1,7 @@
 use anchor_lang::prelude::*;
 use pyth_solana_receiver_sdk::price_update::{get_feed_id_from_hex, Price, PriceUpdateV2};
 
-use crate::constants::{NAV_DECIMALS, PYTH_PRICE_FEED_MAX_AGE_SECONDS};
+use crate::constants::{PRECISION, PYTH_PRICE_FEED_MAX_AGE_SECONDS};
 use crate::state::VaultError;
 
 pub fn get_token_price_from_pyth_feed<'info>(
@@ -27,6 +27,8 @@ pub fn transform_price_to_nav_decimals(price: &Price) -> Result<u64> {
     Ok(price
         .price
         .unsigned_abs()
-        .checked_mul(10u64.pow((NAV_DECIMALS - (-price.exponent) as u8) as u32))
+        .checked_mul(PRECISION)
+        .ok_or(VaultError::NumericOverflow)?
+        .checked_div(10u64.pow((-price.exponent) as u32))
         .ok_or(VaultError::NumericOverflow)?)
 }
