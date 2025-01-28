8b550cd9ac26d376a066cc494155f811c67074e6
Sounak Pradhan <22390515+sounak98@users.noreply.github.com>
2025-01-28 15:09:56 +0100

    feat: add JUP pyth price feed id

--- tests/utils.new/constants.ts
+++ tests/utils.new/constants.ts
@@ -1,7 +1,10 @@
 export const DEFAULT_MINT_DECIMALS = 6;
 
-export const DUMMY_PRICE_FEED_ID =
+export const USDC_PRICE_FEED_ID =
   "0xeaa020c61cc479712813461ce153894a96a6c00b21ed0cfc2798d1f9a9e9c94a";
+
+export const JUP_PRICE_FEED_ID =
+  "0x0a0408d619e9380abad35060f9192039ed5042fa6f82301d0e48bb52be830996";
 
 // Connection
 export const DEFAULT_COMMITMENT = "confirmed";
