7ab9a99d6562959b28bb32bab4ffa6e77a59d8ea
Sounak Pradhan <sounak@starke.finance>
2025-04-11 21:54:58 +0200

    feat: update VaultsSDK to support new swap functionality with Jupiter; refactor swap method parameters and enhance address lookup table handling; add utility functions for instruction construction

---
+++ sdk-ts/lib/solana.ts
@@ -0,0 +1,26 @@
+import {
+  AddressLookupTableAccount,
+  Connection,
+  PublicKey,
+} from "@solana/web3.js";
+
+export async function getAddressLookupTables(
+  connection: Connection,
+  addressLookupTableAddresses: string[]
+): Promise<AddressLookupTableAccount[]> {
+  const keys = addressLookupTableAddresses.map(
+    (address) => new PublicKey(address)
+  );
+  const addressLookupTableAccountInfos =
+    await connection.getMultipleAccountsInfo(keys);
+
+  return addressLookupTableAccountInfos.map((info, index) => {
+    const key = keys[index];
+    if (info) {
+      return new AddressLookupTableAccount({
+        key,
+        state: AddressLookupTableAccount.deserialize(info.data),
+      });
+    }
+  });
+}
