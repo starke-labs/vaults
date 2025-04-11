6cd42d9e08aed90a96fb3718811cb697595e285c
Sounak Pradhan <sounak@starke.finance>
2025-04-11 21:59:17 +0200

    feat: add unit tests for swap functionality in VaultsSDK; implement test for swapping USDC to USDT using Jupiter

---
+++ tests/vaults/swap.test.ts
@@ -0,0 +1,31 @@
+import { BN } from "@coral-xyz/anchor";
+import { Connection, Keypair } from "@solana/web3.js";
+
+import { VaultsSDK } from "@starke/sdk";
+import { USDC, USDT } from "@starke/sdk/whitelist";
+
+import { createConnection, getManagerKeypair } from "../utils.new";
+
+describe("Swap", () => {
+  let connection: Connection;
+  let vaults: VaultsSDK;
+  let manager: Keypair;
+
+  before(async () => {
+    connection = createConnection();
+    manager = getManagerKeypair();
+    vaults = new VaultsSDK(connection, manager);
+  });
+
+  it("should test something", async () => {
+    console.log(manager.publicKey.toBase58());
+
+    const tx = await vaults.swapOnJupiter(
+      USDC.mint,
+      USDT.mint,
+      new BN(1 * 10 ** 6),
+      manager.publicKey
+    );
+    console.log(tx);
+  });
+});
