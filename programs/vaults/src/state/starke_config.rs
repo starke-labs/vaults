4fbf06d1065cf4cb9231fd51b78697afb7938953
Sounak Pradhan <sounak@starke.finance>
2025-04-21 13:47:04 +0200

    fix(KS-SRK-F-02): Emergency Shutdown Not Present

---
+++ programs/vaults/src/state/starke_config.rs
@@ -0,0 +1,32 @@
+use anchor_lang::prelude::*;
+
+#[account]
+pub struct StarkeConfig {
+    pub bump: u8,
+    pub is_paused: bool,
+}
+
+impl StarkeConfig {
+    pub const MAX_SPACE: usize = 8 + 1 + 1;
+
+    pub const SEED: &'static [u8] = b"STARKE_CONFIG";
+
+    pub fn initialize(&mut self, bump: u8) -> Result<()> {
+        self.bump = bump;
+        self.is_paused = false;
+        Ok(())
+    }
+
+    pub fn update_pause_status(&mut self, is_paused: bool) -> Result<()> {
+        self.is_paused = is_paused;
+        Ok(())
+    }
+}
+
+#[error_code]
+pub enum StarkeConfigError {
+    #[msg("Unauthorized")]
+    Unauthorized,
+    #[msg("Starke is paused")]
+    StarkePaused,
+}
