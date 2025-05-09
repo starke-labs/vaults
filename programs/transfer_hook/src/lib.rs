1bac916f144490891b67b0831b512a9de4a5d9e6
Sounak Pradhan <sounak@starke.finance>
2025-05-09 21:10:15 +0200

    refactor: rename EXTRA_ACCOUNT_META_SEED to EXTRA_ACCOUNT_METAS_SEED and update related references; streamline VaultConfig structure and methods for improved clarity and functionality

--- programs/transfer_hook/src/lib.rs
+++ programs/transfer_hook/src/lib.rs
@@ -9,8 +9,8 @@ use spl_transfer_hook_interface::instruction::ExecuteInstruction;
 pub mod constants;
 pub mod state;
 
-use constants::EXTRA_ACCOUNT_META_SEED;
-use state::VaultConfigs;
+use constants::EXTRA_ACCOUNT_METAS_SEED;
+use state::VaultConfig;
 
 declare_id!("3Mbtr8yzqLUuBZVSefrVtAPmgNLFutEXeRWJNATsKU5z");
 
@@ -26,128 +26,132 @@ pub enum TransferHookError {
 
 #[program]
 pub mod transfer_hook {
+    use spl_transfer_hook_interface::instruction::TransferHookInstruction;
+
     use super::*;
 
-    pub fn initialize_extra_account_metas(
-        ctx: Context<InitializeExtraAccountMetaAccounts>,
-    ) -> Result<()> {
-        let vault_configs = &mut ctx.accounts.vault_configs;
-        vault_configs.initialize(ctx.bumps.vault_configs)?;
-
-        let extra_account_metas = InitializeExtraAccountMetaAccounts::extra_account_metas()?;
-        ExtraAccountMetaList::init::<ExecuteInstruction>(
-            &mut ctx.accounts.extra_account_meta_list.try_borrow_mut_data()?,
-            &extra_account_metas,
-        )?;
-
-        Ok(())
-    }
-
     // #[instruction(discriminator = b"spl-transfer-hook-interface:initialize-extra-account-metas")]
-    pub fn add_vault_config(
-        ctx: Context<InitializeExtraAccountMetaAccounts>,
+    pub fn initialize_extra_account_metas(
+        ctx: Context<InitializeExtraAccountMetasAccounts>,
         vtoken_is_transferrable: bool,
     ) -> Result<()> {
-        msg!("add_vault_config {}", vtoken_is_transferrable);
-
-        let vault_configs = &mut ctx.accounts.vault_configs;
-        vault_configs.add_vault_config(
+        let vault_config = &mut ctx.accounts.vault_config;
+        vault_config.initialize(
             // &ctx.accounts.vault.key(),
-            &pubkey!("3Mbtr8yzqLUuBZVSefrVtAPmgNLFutEXeRWJNATsKU5z"),
+            // &ctx.accounts.vault.manager,
+            // &ctx.accounts.vault.mint.key(),
+            &ctx.accounts.mint.key(),
             &ctx.accounts.manager.key(),
             &ctx.accounts.mint.key(),
             vtoken_is_transferrable,
-            ctx.bumps.vault_configs,
+            ctx.bumps.vault_config,
         )?;
 
-        let extra_account_metas = InitializeExtraAccountMetaAccounts::extra_account_metas()?;
+        let extra_account_metas = InitializeExtraAccountMetasAccounts::extra_account_metas(
+            // &ctx.accounts.vault.mint.key(),
+        )?;
         ExtraAccountMetaList::init::<ExecuteInstruction>(
-            &mut ctx.accounts.extra_account_meta_list.try_borrow_mut_data()?,
+            &mut ctx.accounts.extra_account_metas.try_borrow_mut_data()?,
             &extra_account_metas,
         )?;
 
         Ok(())
     }
 
-    #[instruction(discriminator = b"spl-transfer-hook-interface:execute")]
+    // #[instruction(discriminator = b"spl-transfer-hook-interface:execute")]
     pub fn execute(ctx: Context<ExecuteAccounts>, _amount: u64) -> Result<()> {
-        let vault_configs = &mut ctx.accounts.vault_configs;
-        let vtoken_is_transferrable =
-            vault_configs.check_if_vtoken_is_transferrable(&ctx.accounts.mint.key())?;
-        require!(
-            vtoken_is_transferrable,
-            TransferHookError::TokenNonTransferrable
-        );
+        // TODO: Uncomment this once we have fixed error:
+        // AnchorError caused by account: vault_config. Error Code: AccountDiscriminatorMismatch.
+        // Error Number: 3002. Error Message: Account discriminator did not match what was expected.
+
+        // let vault_config = &ctx.accounts.vault_config;
+        // let vtoken_is_transferrable = vault_config.check_if_vtoken_is_transferrable()?;
+        // require!(
+        //     vtoken_is_transferrable,
+        //     TransferHookError::TokenNonTransferrable
+        // );
 
         Ok(())
+    }
+
+    /// Fallback instruction handler as workaround to anchor instruction discriminator check
+    pub fn fallback<'info>(
+        program_id: &Pubkey,
+        accounts: &'info [AccountInfo<'info>],
+        data: &[u8],
+    ) -> Result<()> {
+        let instruction = TransferHookInstruction::unpack(data)?;
+        // Match instruction discriminator to transfer hook interface execute instruction
+        // Token2022 program CPIs this instruction on token transfer
+        match instruction {
+            TransferHookInstruction::Execute { amount } => {
+                let amount_bytes = amount.to_le_bytes();
+                // Invoke custom transfer hook instruction on our program
+                __private::__global::execute(program_id, accounts, &amount_bytes)
+            }
+            _ => return Err(ProgramError::InvalidInstructionData.into()),
+        }
     }
 
     pub fn set_vtoken_is_transferrable(
         ctx: Context<SetVtokenIsTransferrableAccounts>,
         vtoken_is_transferrable: bool,
     ) -> Result<()> {
-        let vault_configs = &mut ctx.accounts.vault_configs;
-        vault_configs
-            .set_vtoken_is_transferrable(&ctx.accounts.mint.key(), vtoken_is_transferrable)?;
+        let vault_config = &mut ctx.accounts.vault_config;
+        vault_config.set_vtoken_is_transferrable(vtoken_is_transferrable)?;
 
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct InitializeExtraAccountMetaAccounts<'info> {
+pub struct InitializeExtraAccountMetasAccounts<'info> {
     // Payer is the manager of the vault
     #[account(mut)]
     manager: Signer<'info>,
 
     /// CHECK: ExtraAccountMetaList Account, must use these seeds
     #[account(
-        init_if_needed,
-        seeds = [EXTRA_ACCOUNT_META_SEED, mint.key().as_ref()],
+        init,
+        seeds = [EXTRA_ACCOUNT_METAS_SEED, mint.key().as_ref()],
         bump,
         space = ExtraAccountMetaList::size_of(
-            InitializeExtraAccountMetaAccounts::extra_account_metas()?.len()
+            InitializeExtraAccountMetasAccounts::extra_account_metas()?.len()
         )?,
         payer = manager
     )]
-    pub extra_account_meta_list: UncheckedAccount<'info>,
-
-    #[account(
-        init,
-        seeds = [VaultConfigs::SEED, mint.key().as_ref()],
-        bump,
-        payer = manager,
-        space = VaultConfigs::MAX_SPACE
-    )]
-    pub vault_configs: Account<'info, VaultConfigs>,
-
-    pub system_program: Program<'info, System>,
-}
-
-// Define extra account metas to store on extra_account_meta_list account
-impl<'info> InitializeExtraAccountMetaAccounts<'info> {
-    pub fn extra_account_metas() -> Result<Vec<ExtraAccountMeta>> {
-        msg!("extra_account_metas");
-        Ok(vec![ExtraAccountMeta::new_with_seeds(
-            &[Seed::Literal {
-                bytes: VaultConfigs::SEED.to_vec(),
-            }],
-            false, // is_signer
-            true,  // is_writable
-        )?])
-    }
-}
-
-#[derive(Accounts)]
-pub struct AddVaultConfigAccounts<'info> {
-    #[account(mut)]
-    manager: Signer<'info>,
+    pub extra_account_metas: UncheckedAccount<'info>,
 
     // #[account(
     //     constraint = vault.manager == manager.key() @ TransferHookError::Unauthorized,
     // )]
     // pub vault: Account<'info, Vault>,
     pub mint: InterfaceAccount<'info, Mint>,
+
+    #[account(
+        init,
+        // seeds = [VaultConfig::SEED, mint.key().as_ref()],
+        seeds = [VaultConfig::SEED],
+        bump,
+        payer = manager,
+        space = VaultConfig::MAX_SPACE
+    )]
+    pub vault_config: Account<'info, VaultConfig>,
+
+    pub system_program: Program<'info, System>,
+}
+
+// Define extra account metas to store on extra_account_meta_list account
+impl<'info> InitializeExtraAccountMetasAccounts<'info> {
+    pub fn extra_account_metas() -> Result<Vec<ExtraAccountMeta>> {
+        Ok(vec![ExtraAccountMeta::new_with_seeds(
+            &[Seed::Literal {
+                bytes: VaultConfig::SEED.to_vec(),
+            }],
+            false, // is_signer
+            false, // is_writable
+        )?])
+    }
 }
 
 // Order of accounts matters for this struct
@@ -166,13 +170,14 @@ pub struct ExecuteAccounts<'info> {
 
     /// CHECK: Source token account owner, can be SystemAccount or PDA owned by another program
     pub owner: UncheckedAccount<'info>,
-
-    /// CHECK: ExtraAccountMetaList Account, must use these seeds
-    #[account(seeds = [EXTRA_ACCOUNT_META_SEED, mint.key().as_ref()], bump)]
-    pub extra_account_meta_list: UncheckedAccount<'info>,
-
-    #[account(seeds = [VaultConfigs::SEED], bump = vault_configs.bump)]
-    pub vault_configs: Account<'info, VaultConfigs>,
+    #[account(
+        // We do not mutate the vault_config account in the ExecuteAccounts context
+        // mut,
+        // seeds = [VaultConfig::SEED, mint.key().as_ref()],
+        seeds = [VaultConfig::SEED],
+        bump = vault_config.bump
+    )]
+    pub vault_config: Account<'info, VaultConfig>,
 }
 
 #[derive(Accounts)]
@@ -186,6 +191,11 @@ pub struct SetVtokenIsTransferrableAccounts<'info> {
     // pub vault: Account<'info, Vault>,
     pub mint: InterfaceAccount<'info, Mint>,
 
-    #[account(mut, seeds = [VaultConfigs::SEED, mint.key().as_ref()], bump = vault_configs.bump)]
-    vault_configs: Account<'info, VaultConfigs>,
+    #[account(
+        mut,
+        // seeds = [VaultConfig::SEED, mint.key().as_ref()],
+        seeds = [VaultConfig::SEED],
+        bump = vault_config.bump
+    )]
+    vault_config: Account<'info, VaultConfig>,
 }
