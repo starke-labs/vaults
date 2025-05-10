4bd6764d929dd01b7c97abddba48dea1524bc561
Sounak Pradhan <sounak@starke.finance>
2025-05-10 16:28:03 +0200

    refactor: rename transfer_hook package to use hyphen and update VaultConfig to VtokenConfig to not depend on vaults package thus avoiding cyclic dependency

--- programs/transfer_hook/src/state.rs
+++ programs/transfer_hook/src/state.rs
@@ -2,29 +2,25 @@ use anchor_lang::prelude::*;
 
 #[account]
 #[derive(InitSpace)]
-pub struct VaultConfig {
-    pub key: Pubkey,
+pub struct VtokenConfig {
     pub manager: Pubkey,
-    // Do we need to store the vtoken mint?
     pub vtoken_mint: Pubkey,
     pub vtoken_is_transferrable: bool,
     pub bump: u8,
 }
 
-impl VaultConfig {
-    pub const SEED: &'static [u8] = b"STARKE_VAULT_CONFIG";
+impl VtokenConfig {
+    pub const SEED: &'static [u8] = b"STARKE_VTOKEN_CONFIG";
     pub const MAX_SPACE: usize = 8 + // Discriminator
-        VaultConfig::INIT_SPACE;
+        VtokenConfig::INIT_SPACE;
 
     pub fn initialize(
         &mut self,
-        vault_key: &Pubkey,
         manager: &Pubkey,
         vtoken_mint: &Pubkey,
         vtoken_is_transferrable: bool,
         bump: u8,
     ) -> Result<()> {
-        self.key = *vault_key;
         self.manager = *manager;
         self.vtoken_mint = *vtoken_mint;
         self.vtoken_is_transferrable = vtoken_is_transferrable;
