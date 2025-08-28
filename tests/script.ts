fb789cda2c3837f69fecb50c2fc12dc2f48a34cd
Sounak Pradhan <sounak@starke.finance>
2025-08-28 21:31:06 +0200

    feat: enhance test script with multiple vault instances and deposit functionality

--- tests/script.ts
+++ tests/script.ts
@@ -1,29 +1,36 @@
 import { VaultsSdk } from "@starke/sdk";
 import { USDC } from "@starke/sdk/whitelist";
 
-import { createConnection, getManagerKeypair } from "./utils.new";
+import {
+  createConnection,
+  getAuthorityKeypair,
+  getManagerKeypair,
+  getTesterKeypair,
+  toTokenAmount,
+} from "./utils.new";
 
 const manager = getManagerKeypair();
+const tester = getTesterKeypair();
+const authority = getAuthorityKeypair();
 
 const RPC_URL =
   "https://mainnet.helius-rpc.com/?api-key=722d0f89-704b-4963-a20f-cdddda7c8798";
 
-const vaults = new VaultsSdk(createConnection(RPC_URL), manager);
+const vaults = {
+  manager: new VaultsSdk(createConnection(RPC_URL), manager),
+  tester: new VaultsSdk(createConnection(RPC_URL), tester),
+  authority: new VaultsSdk(createConnection(RPC_URL), authority),
+};
 
 (async () => {
   // Write your script here
-
-  // Deposit USDC to the vault
-
-  // Get the vault
-  const vault = await vaults.fetchVault(manager.publicKey);
-  console.log(vault);
-
-  // Get the vault's vtoken mint
-  const vTokenMint = vault.mint;
-  console.log(vTokenMint);
-
-  // Create a vault
+  // // Get the vault
+  // const vault = await vaults.manager.fetchVault(manager.publicKey);
+  // console.log(vault);
+  // // Get the vault's vtoken mint
+  // const vTokenMint = vault.mint;
+  // console.log(vTokenMint);
+  // // Create a vault
   // const vault = await vaults.createVault(
   //   "test vault",
   //   "TV",
@@ -37,4 +44,13 @@ const vaults = new VaultsSdk(createConnection(RPC_URL), manager);
   //   true,
   //   true
   // );
+
+  // Deposit USDC to the vault
+  const deposit = await vaults.tester.deposit(
+    toTokenAmount(1),
+    tester.publicKey,
+    manager.publicKey,
+    [authority]
+  );
+  console.log(deposit);
 })();
