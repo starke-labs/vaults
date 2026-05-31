d1286ce17f4f4e2150c56a9de0ffe8098c66ee1d
Sounak Pradhan <sounak@starke.finance>
2026-05-31 13:35:52 +0530

    chore: update pyth price feed max age

--- programs/vaults/src/constants.rs
+++ programs/vaults/src/constants.rs
@@ -3,7 +3,7 @@ use anchor_lang::prelude::*;
 pub static STARKE_AUTHORITY: Pubkey = pubkey!("STRK1me6eFLDYGKYqbn2oyHsaxiCHe8GDWQnnSGiScS");
 
 // Can be as low as 60 seconds because we are using the Pyth sponsored price feed
-pub static PYTH_PRICE_FEED_MAX_AGE_SECONDS: u64 = 75;
+pub static PYTH_PRICE_FEED_MAX_AGE_SECONDS: u64 = 120;
 
 // 100 basis points = 1%
 pub static PYTH_CONFIDENCE_THRESHOLD_BPS: u64 = 100;
